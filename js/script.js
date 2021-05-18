cd1561f72e9a4822627c08f3e7632772ce149235
fmarabini93 <fmarabini@icloud.com>
2021-05-18 15:50:49 +0200

    autoplay

--- js/script.js
+++ js/script.js
@@ -3,14 +3,23 @@ const app = new Vue (
       {
 
       el: "#root",
+
       data: {
             imgs: {
+
                   url: ["img/img1.jpg", "img/img2.jpg", "img/img3.jpg", "img/img4.jpg"],
                   alt: ["Paesaggio1", "Paesaggio2", "Paesaggio3", "Paesaggio4"]
+            
             },
             imgIndex: 0
       },
+
+      mounted: function() { // --> Slideshow 3s autoplay
+            setInterval(this.nextImage, 3000);
+      },
+      
       methods: {
+
             prevImage: function() {
                   this.imgIndex--;
                   if (this.imgIndex < 0) {
@@ -23,6 +32,7 @@ const app = new Vue (
                   if (this.imgIndex == this.imgs.url.length) {
                         this.imgIndex = 0;
                   }
+            
             }
       }
 
